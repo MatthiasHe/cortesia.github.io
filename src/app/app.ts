4f0b589921cf7f3835f582317be1deaa3ee8c618
Matthias He <matthias.heuveline@skillcorner.com>
2026-01-25 17:38:25 +0100

    Test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, HostListener, Renderer2, inject, DOCUMENT } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+  private readonly renderer = inject(Renderer2);
+  private readonly document = inject(DOCUMENT);
+
   protected readonly title = signal('cortesia');
+  protected readonly isMenuOpen = signal(false);
+
+  protected openMenu(): void {
+    this.isMenuOpen.set(true);
+    this.renderer.setStyle(this.document.body, 'overflow', 'hidden');
+  }
+
+  protected closeMenu(): void {
+    this.isMenuOpen.set(false);
+    this.renderer.setStyle(this.document.body, 'overflow', '');
+  }
+
+  @HostListener('document:keydown.escape')
+  protected onEscapeKey(): void {
+    if (this.isMenuOpen()) {
+      this.closeMenu();
+    }
+  }
 }
